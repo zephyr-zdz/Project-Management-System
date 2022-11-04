0db51ac2bba298f20c8fc46af4759c8f4d9021b3
zhoudazhi <dazhi527@hotmail.com>
2022-11-05 00:04:03 +0800

    edit 登录参数

--- src/main/java/com/example/projectmanagementsystem/controller/user/UserController.java
+++ src/main/java/com/example/projectmanagementsystem/controller/user/UserController.java
@@ -1,37 +1,31 @@
 package com.example.projectmanagementsystem.controller.user;
 
-import com.example.projectmanagementsystem.controller.config.WebMvcConfig;
 import com.example.projectmanagementsystem.model.entity.User;
 import com.example.projectmanagementsystem.model.vo.SafeUser;
 import com.example.projectmanagementsystem.service.user.UserService;
 import com.example.projectmanagementsystem.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @RestController("UserUserController")
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
-    private final HttpServletRequest request;
 
     @Autowired
     UserController(UserService userService, HttpServletRequest request) {
         this.userService = userService;
-        this.request = request;
     }
 
     @PostMapping("/login")
-    public Response<String> login(HttpSession session) {
-        String name = request.getParameter("username");
-        String password = request.getParameter("password");
-        Response<String> answer = userService.login(name, password);
-        if (answer.getData().equals("true")) {    // 登录成功
-            session.setAttribute("user", WebMvcConfig.SESSION_KEY); // 登录之后，session就有了user的值
-        }
-        return answer;
+    public Response<String> login(@RequestParam("username") String name,
+                                  @RequestParam("password") String password) {
+        return userService.login(name, password);
     }
 
     @PostMapping("/register")
