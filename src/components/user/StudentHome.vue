0dc80d00ea1c63fcf29ac681ffaa14aa45d196c9
windyyy217 <2422314671@qq.com>
2022-11-06 22:24:48 +0800

    个人信息

---
+++ src/components/user/StudentHome.vue
@@ -0,0 +1,35 @@
+<!--2022/3/2 created by Yifan Yu-->
+<template>
+  <div>
+    <el-container>
+      <el-header height="auto"><student-nav-bar></student-nav-bar></el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import StudentNavBar from "./StudentNavBar";
+export default {
+  name: "StudentHome",
+  components: {StudentNavBar}
+}
+</script>
+
+<style scoped>
+.el-header {
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+
+.el-main {
+  height: 90vh;
+  text-align: center;
+  line-height: 160px;
+  padding: 0;
+}
+</style>
