24bed39fdfc2f71975907821b14ae1745fb4f537
zhoudazhi <dazhi527@hotmail.com>
2022-11-14 16:22:19 +0800

    add 增加response返回参数

--- src/main/java/com/example/projectmanagementsystem/util/Response.java
+++ src/main/java/com/example/projectmanagementsystem/util/Response.java
@@ -11,9 +11,9 @@ public class Response<T> {
     //    public static final int NO_LOGIN = -1;
     public static final int SUCCESS = 0;
     public static final int FAIL = 1;
-//    public static final int ERROR_ROLE = 2;
+    public static final int NOT_FOUND = 2;
 //    public static final int NO_PERMISSION = 3;
-//    public static final int INVALID_PARAMETER = 4;
+    public static final int INVALID_PARAMETER = 4;
 //    public static final int SERVER_ERROR = 10;
 //    private static final long serialVersionUID = 1L;
 
