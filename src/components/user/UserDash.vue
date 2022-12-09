bf344a7ad94a9a33183ee691d29c999aa8dfe5c7
TTTTTTi <1273745709@qq.com>
2022-12-09 16:44:01 +0800

    修改图片路径

--- src/components/user/UserDash.vue
+++ src/components/user/UserDash.vue
@@ -4,7 +4,7 @@
       <el-main>
         <section id="banner">
           <div id="main-slider" class="flexslider">
-            <img src="../assets/title.jpg" alt="flexslider" />
+            <img src="../../assets/title.jpg" alt="flexslider" />
             <div class="flex-caption" style="margin-left: 18%">
               <div style="font-size: 70px;background-color:rgba(255,255,255,0.5);margin-right:45%;width: 375px">
                 <h3 style="font-size: 60px;color:#204F9F; opacity: 100%;">项目管理系统</h3>
